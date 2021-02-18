2d1125825df276fe8f2c03cc998625034ae47a6a
Brian Cook <brybcook@gmail.com>
2021-02-18 20:33:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,11 +36,12 @@ end
 
 def hit?(total)
   # code hit? here
+  card_total=initial_round
   prompt_user
-  get_user_input
-  if get_user_input=="s"
+  user_input=get_user_input
+  if user_input=="s"
     card_total
-  elsif get_user_input=="h"
+  elsif user_input=="h"
     card_total+=deal_card
   else
     invalid_command
