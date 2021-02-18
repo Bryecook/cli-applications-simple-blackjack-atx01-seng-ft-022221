b48db72c71450703b06d602b5d3216ca3dc75cdb
Brian Cook <brybcook@gmail.com>
2021-02-18 20:19:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,8 +29,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card+deal_card
-  display_card_total(deal_card+deal_card)
+  deal_card+deal_card=card_total
+  display_card_total(card_total)
   puts deal_card+deal_card
 end
 
