4854b98bf469e9a038bad7872080e7fcd921cbd2
Brian Cook <brybcook@gmail.com>
2021-02-18 20:35:42 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,13 +44,13 @@ def hit?(total)
     total+=deal_card
   else
     invalid_command
-    prompt_user
   end
 end
 
 def invalid_command
   # code invalid_command here
   puts "Please enter a valid command"
+  get_user_input
 end
 
 #####################################################
